e2308eebd9196e0924748a76b5bc65332b75a2bd
winZD <puntica007@gmail.com>
2024-06-29 16:41:48 +0200

    Added survey

---
+++ src/components/Survey.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Survey = () => {
+  return (
+    <div style={{ border: "1px solid black", padding: 20 }}>
+      <p>Jeste li zadovoljni primanjima?</p>
+      <button>Da</button>
+      <button>Ne</button>
+    </div>
+  );
+};
+
+export default Survey;

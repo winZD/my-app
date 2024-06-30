a41ccba86843023208815682ca1765d12e86789b
winZD <puntica007@gmail.com>
2024-06-30 15:39:41 +0200

    r

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Survey = ({ props }: { props: number }) => {
   console.log(props);
+  const [data, setData] = useState({ description: "" });
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${Math.floor(Math.random() * 10)}`)
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  /* { status: 'ok', method: 'GET' } */
   const navigate = useNavigate();
   return (
     <div style={{ border: "1px solid black", padding: 20 }}>
-      <p>Jeste li zadovoljni primanjima?{props}</p>
+      <p>{data.description}</p>
       <button
         onClick={() => {
           alert("You clicked on yes");
